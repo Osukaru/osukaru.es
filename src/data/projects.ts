187a32bff7c8ea059b7243cb97141d7c434c1834
Osukaru <zepolracso@gmail.com>
2025-08-10 13:08:02 +0200

    Updated projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -15,21 +15,5 @@ export const projects: Project[] = [
     description: 'Mi web personal con diseño retro inspirado en el Sinclair Spectrum ZX 128K +2A. Desarrollada con Astro, TypeScript y CSS personalizado para recrear la estética de los años 80.',
     url: 'https://www.osukaru.es',
     githubUrl: 'https://github.com/Osukaru/osukaru.es'
-  },
-  {
-    id: 'osukaru-web2',
-    name: 'Osukaru.es',
-    logo: '/images/projects/osukaru-web-logo.png',
-    description: 'Mi web personal con diseño retro inspirado en el Sinclair Spectrum ZX 128K +2A. Desarrollada con Astro, TypeScript y CSS personalizado para recrear la estética de los años 80.',
-    url: 'https://www.osukaru.es',
-    githubUrl: 'https://github.com/Osukaru/osukaru.es'
-  },
-  {
-    id: 'osukaru-web3',
-    name: 'Osukaru.es',
-    logo: '/images/projects/osukaru-web-logo.png',
-    description: 'Mi web personal con diseño retro inspirado en el Sinclair Spectrum ZX 128K +2A. Desarrollada con Astro, TypeScript y CSS personalizado para recrear la estética de los años 80.',
-    url: 'https://www.osukaru.es',
-    githubUrl: 'https://github.com/Osukaru/osukaru.es'
   }
 ];
